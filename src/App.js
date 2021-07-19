6f0ebb264632030892beb76e56dce441bc016dfd
kenneth adjei-mensah <kenadjei20@gmail.com>
2021-07-19 09:26:21 +0000

    app.js update

--- src/App.js
+++ src/App.js
@@ -4,26 +4,21 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      users:  [],
+      users: [],
     };
-   
-      
-    
+
     this.handleChange = (e) => {
       this.setState = { [e.target.name]: e.target.value };
-   
     };
-   
   }
-  
-  
+
   render() {
-return (
+    return (
       <div>
         <Form adduser={this.handlenewUser} />
       </div>
     );
-  };
+  }
 }
 
 export default App;
