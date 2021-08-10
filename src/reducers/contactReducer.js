15f5db177c192892b40f0a164f9386ad0f951f76
kenneth adjei-mensah <kenadjei20@gmail.com>
2021-08-10 21:48:39 +0000

    containerReducer.js

---
+++ src/reducers/contactReducer.js
@@ -0,0 +1,15 @@
+
+const initialstate = {
+  users: []
+};
+const contactReducer = (state = { initialstate}, action ) => {
+  switch (action.type) {
+    case state:
+      break;
+
+    default:
+      break;
+  }
+};
+
+export default contactReducer;
